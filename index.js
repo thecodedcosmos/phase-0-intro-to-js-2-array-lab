06ce1ed3249378373d66cd813f947972d5d040b9
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-04 06:58:28 -0700

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+   return cats.push ("Ralph"); 
+}
+function destructivelyPrependCat() {
+   return cats.unshift ("Bob"); 
+}
+function destructivelyRemoveLastCat() {
+   return cats.pop ("Garfield"); 
+}
+function destructivelyRemoveFirstCat() {
+   return cats.shift ("Milo"); 
+}
+function appendCat() {
+   return [...cats, "Broom"]; 
+}
+function prependCat() {
+   return ["Arnold", ...cats]; 
+}
+function removeLastCat() {
+   return cats.slice(0, cats.length - 1); 
+}
+function removeFirstCat() {
+   return cats.slice(1); 
+}
